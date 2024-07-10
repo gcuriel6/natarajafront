ebd4128d6ca28f470b0c36d7f9005ad57e425ed3
Gabriel Curiel <curiel.mayagoitia@gmail.com>
2024-07-10 11:13:23 -0600

    boton docente nuevo

--- app/(views)/(private)/docentes/page.jsx
+++ app/(views)/(private)/docentes/page.jsx
@@ -1,12 +1,18 @@
 import Datatable from "@/app/_components/Datatable/datatable";
 import { docentescolumns as columns } from "@/app/_components/Datatable/Columns/docentes";
 import { fetchRequest } from "@/app/_lib/actions";
+import BotonNuevo from "./Botones/botonNuevo";
 
 export default async function Page() {
     const data = await fetchRequest("/docentes", "GET", null);
   
     if(data.error){
-      return (<>{data.message}</>)
+      return (
+        <>
+          <h1>{data.message}</h1>
+          <BotonNuevo/>
+        </>
+      )
     }
   
     const docentes = data.message;
